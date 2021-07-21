8841005962c56aec11b2d3e032d0fa3175307fa8
Milan Kumal <86836771+Milan3341@users.noreply.github.com>
2021-07-21 22:38:19 +1000

    second change

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(Quizzler());
@@ -5,6 +7,33 @@ void main() => runApp(Quizzler());
 class Quizzler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Scaffold());
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(child: Quizpage()),
+      ),
+    );
+  }
+}
+
+class Quizpage extends StatefulWidget {
+  const Quizpage({Key key}) : super(key: key);
+
+  @override
+  _QuizpageState createState() => _QuizpageState();
+}
+
+class _QuizpageState extends State<Quizpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          child: Text(
+            'You can lead a cow down stairs but not up stairs.',
+            style: TextStyle(color: Colors.white, fontSize: 25),
+          ),
+        ),
+      ],
+    );
   }
 }
